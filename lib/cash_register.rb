38365412c4066cbeea6ad61ccfb9c088bf7a8125
Eli Cleveland <elicleveland12@gmail.com>
2019-01-12 16:42:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
-
+class CashRegister
+  
+   attr_accessor :total, :discount
+  
+  def initialize(total = 0, discount = 20)
+    @total = total
+    @discount = discount
+  end 
+  
+  def add_item(item, price)
+    
+  end 
+  
+end
